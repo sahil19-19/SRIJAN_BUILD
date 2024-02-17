d1b65f367446542693b34c5731b2aee0aadcf95d
Sahil <sahilverma1931@gmail.com>
2024-02-17 20:26:03 +0530

    add link to images in editionsPhone.js file

--- src/Pages/Editions/EditionPhone.js
+++ src/Pages/Editions/EditionPhone.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 import "../../Assets/css/EditionPhone.css";
-import first from "../../Assets/images/first.jpg";
-import second from "../../Assets/images/second.jpg";
+import image2020 from "../../Assets/images/first.jpg";
+import image2022 from "../../Assets/images/second.jpg";
 import image2021 from "../../Assets/images/2020-2021.png";
-import image2016 from "../../Assets/images/2016-2017.png";
+import image2018 from "../../Assets/images/2016-2017.png";
 import image2017 from "../../Assets/images/2017-2018.png";
-import image2018 from "../../Assets/images/2018-2019.png";
+import image2019 from "../../Assets/images/2018-2019.png";
 import Navbar from "../../components/NewNavbar.js/NewNavbar";
 
 import Carousel from "react-elastic-carousel";
@@ -17,31 +17,56 @@ const EditionPhone = () => {
       <section className="phone-edition">
         <div className="current-edition">
           <div className="edition-text">SRIJAN 2021-22</div>
-          <img className="edition-img" src={second} alt="edition"></img>
+          <a href="#">
+            <img className="edition-img" 
+            src={image2022} 
+            alt="edition"
+            ></img>
+          </a>
         </div>
         <Carousel itemsToShow={3}>
-          <img className="carousel-components" src={second} alt="edition"></img>
-          <img
-            className="carousel-components"
-            src={image2021}
-            alt="edition"
-          ></img>
-          <img className="carousel-components" src={first} alt="edition"></img>
-          <img
-            className="carousel-components"
-            src={image2018}
-            alt="edition"
-          ></img>
-          <img
-            className="carousel-components"
-            src={image2016}
-            alt="edition"
-          ></img>
-          <img
-            className="carousel-components"
-            src={image2017}
-            alt="edition"
-          ></img>
+          <a href="https://drive.google.com/file/d/1lqy5vTkv4erXBLndVKP5PuBxHZ-To3NR/view?usp=sharing>">
+            <img
+              className="carousel-components"
+              src={image2022}
+              alt="edition"
+            ></img>
+          </a>
+          <a href="https://drive.google.com/file/d/1qjMCAEk75KmO9mumi2fra55Ua3XB6qpt/view?usp=sharing">
+            <img
+              className="carousel-components"
+              src={image2021}
+              alt="edition"
+            ></img>
+          </a>
+          <a href="https://drive.google.com/file/d/1lmBbYQvnFDeM7KNqZ6L0J7RtadBBle4E/view?usp=sharing">
+            <img
+              className="carousel-components"
+              src={image2020}
+              alt="edition"
+            ></img>
+          </a>
+          <a href="https://drive.google.com/file/d/1SC6heWxbESkYuql8VBAyVUXlWZ8crl08/view?usp=drive_link">
+            <img
+              className="carousel-components"
+              src={image2019}
+              alt="edition"
+            ></img>
+          </a>
+          <a href="https://drive.google.com/file/d/1yIWkM0lmwmqvQ41xUenTqAfrXdhhb9Nr/view?usp=sharing">
+            <img
+              className="carousel-components"
+              src={image2018}
+              alt="edition"
+            ></img>
+          </a>
+          <a href="https://drive.google.com/file/d/1rag_ijpEnO93SxTg1v_vIi8VYBsSo-nC/view?usp=drive_link">
+            <img
+              className="carousel-components"
+              src={image2017}
+              alt="edition"
+            ></img>
+          </a>
         </Carousel>
       </section>
     </>
